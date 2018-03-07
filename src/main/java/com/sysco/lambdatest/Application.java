91c88f989fd92654cd07f6d87480725109f01ef9
Heshan-Sandamal <mhsandamal1@gmail.com>
2018-03-07 12:07:18 +0530

    Solve the problem of auto configuring datasource, added datasource manual configurations to Application class

--- src/main/java/com/sysco/lambdatest/Application.java
+++ src/main/java/com/sysco/lambdatest/Application.java
@@ -1,14 +1,22 @@
 package com.sysco.lambdatest;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
@@ -24,12 +32,36 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = "com.sysco.lambdatest.*")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@PropertySource("classpath:applicationdb.properties")
 public class Application extends SpringBootServletInitializer {
+
+
+    @Value("${jdbc.url}") String url;
+    @Value ("${jdbc.username}") String userName;
+    @Value ("${jdbc.password}") String password;
+    @Value("${jdbc.driver-class-name}") String driverClass;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.userName);
+        dataSource.setPassword(this.password);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        System.out.println("Initiating the datasource");
+        return new JdbcTemplate(getDataSource());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/sysco/lambdatest/dao/ItemDao.java
+++ src/main/java/com/sysco/lambdatest/dao/ItemDao.java
@@ -9,21 +9,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ItemDao {
 
-//    @Autowired
-    JdbcTemplate jdbcTemplate=new JdbcTemplate();
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
 
-    public int addItem(Item item){return 1;
-//        String sql = "INSERT INTO item (name, category, vendor, stock)"
-//                + " VALUES (?, ?, ?, ?)";
-//        return jdbcTemplate.update(sql,new Object[]{ item.getName(), item.getCategory(), item.getVendor(), item.getStock()});
+    public int addItem(Item item){
+        String sql = "INSERT INTO item (name, category, vendor, stock)"
+                + " VALUES (?, ?, ?, ?)";
+        return jdbcTemplate.update(sql,new Object[]{ item.getName(), item.getCategory(), item.getVendor(), item.getStock()});
     }
 
 
     public String selectItem(int id){
-return "d";
-//        String sql = "Select name FROM item where id=?";
-//        return (String) this.jdbcTemplate.queryForObject(sql,new Object[]{id},String.class);
+        String sql = "Select name FROM item where id=?";
+        return (String) this.jdbcTemplate.queryForObject(sql,new Object[]{id},String.class);
     }
 
 
